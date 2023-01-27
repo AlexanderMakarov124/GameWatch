0442f94ceda1e714083f1459cf5fa7a063d1dc5d
Alexander Makarov <Joker124rus@gmail.com>
2023-01-27 17:53:05 +0700

    Fix routing

--- GameWatch.Frontend/src/app/app-routing.module.ts
+++ GameWatch.Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CreateGameListComponent } from './game-lists/create-game-list/create-game-list.component';
 import { CreateGameComponent } from './games/create-game/create-game.component';
 import { FindGameComponent } from './games/find-game/find-game.component';
@@ -9,7 +8,6 @@ const routes: Routes = [
   { path: 'games/create', component: CreateGameComponent },
   { path: 'gameLists/create', component: CreateGameListComponent },
   { path: 'games/find', component: FindGameComponent },
-  { path: '**', component: AppComponent }
 ];
 
 @NgModule({
