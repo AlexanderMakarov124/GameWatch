d7988353b47cda9756836cd007072633c91ef79f
Alexander Makarov <Joker124rus@gmail.com>
2023-02-09 18:27:04 +0700

    Add validation

--- GameWatch.Frontend/src/app/games/create-game/create-game.component.ts
+++ GameWatch.Frontend/src/app/games/create-game/create-game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { GameList } from 'src/app/game-lists/shared/game-list.model';
 import { GameListService } from 'src/app/game-lists/shared/game-list.service';
 import { Game } from '../shared/game.model';
@@ -11,22 +11,19 @@ import { GameService } from '../shared/game.service';
   styleUrls: ['./create-game.component.css'],
 })
 export class CreateGameComponent implements OnInit {
-  //   createGameForm = new FormGroup({
-  //    name: new FormControl(''),
-  //    genre: new FormControl(''),
-  //    description: new FormControl('')
-  //   });
+  linkRegex = /http|^$/;
 
   createGameForm = this.formBuilder.group({
-    name: ['', Validators.required],
-    genre: ['', Validators.required],
+    name: ['', [Validators.required, Validators.maxLength(75), this.alreadyExistName()]],
+    genre: ['', [Validators.required, Validators.maxLength(50)]],
     description: [''],
-    gameListName: [''],
-    storeLink: [''],
-    downloadLink: ['']
+    gameListName: ['', [this.forbiddenNameValidator(/default/), Validators.required]],
+    storeLink: ['', this.mustBeLinkValidator(this.linkRegex)],
+    downloadLink: ['', this.mustBeLinkValidator(this.linkRegex)],
   });
 
   gameLists: GameList[] = [];
+  games?: Game[];
 
   constructor(
     private gameService: GameService,
@@ -36,10 +33,31 @@ export class CreateGameComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllGameLists();
+    this.getAllGames();
+  }
+
+  get name() {
+    return this.createGameForm.get('name');
+  }
+
+  get genre() {
+    return this.createGameForm.get('genre');
+  }
+
+  get gameListName() {
+    return this.createGameForm.get('gameListName');
+  }
+
+  get storeLink() {
+    return this.createGameForm.get('storeLink');
+  }
+
+  get downloadLink() {
+    return this.createGameForm.get('downloadLink');
   }
 
   create(): void {
-    const game: Game = this.createGameForm.value as Game;
+    const game = this.createGameForm.value as Game;
 
     game.name = game.name.trim();
     game.genre = game.genre.trim();
@@ -55,5 +73,35 @@ export class CreateGameComponent implements OnInit {
 
   getAllGameLists(): void {
     this.gameListService.getAllGameLists().subscribe(gameLists => (this.gameLists = gameLists));
+  }
+
+  getAllGames(): void {
+    this.gameService.getAllGames().subscribe(allGames => (this.games = allGames));
+  }
+
+  /** A name can't match the given regular expression */
+  forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const forbidden = nameRe.test(control.value);
+      return forbidden ? { forbiddenName: { value: control.value } } : null;
+    };
+  }
+  alreadyExistName(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value) {
+        const game = this.games?.find(game => game.name.toLowerCase() == control.value.toLowerCase());
+
+        return game ? { alreadyExistName: { value: control.value } } : null;
+      }
+      return null;
+    };
+  }
+
+  /** Checks that string whether link or not. */
+  mustBeLinkValidator(linkRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const isLink = linkRe.test(control.value);
+      return isLink ? null : { isLink: { value: control.value } };
+    };
   }
 }
