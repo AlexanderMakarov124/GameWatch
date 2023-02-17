e9555ce71574e45b86f32f16a098ff56dc8fc777
Alexander Makarov <Joker124rus@gmail.com>
2023-02-17 12:52:06 +0700

    Add refreshing random game when you in random game page

---
+++ GameWatch.Frontend/src/app/dummy/dummy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy',
+  template: ''
+})
+export class DummyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
